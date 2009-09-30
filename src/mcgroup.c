bb64dedd454869e28f5c183e70b0be82c1968913
Todd Hayton <todd.hayton@gmail.com>
2009-09-30 13:50:53 +0000

    - Added support for joining and leaving IPv6 multicast groups
    git-svn-id: file:///home/svn/smcroute/trunk@39 9463870d-797f-4c04-96bf-d4a431643c59

--- src/mcgroup.c
+++ src/mcgroup.c
@@ -26,8 +26,58 @@
 
 #include "mclab.h"
 
+static int joinleave4( int, int, const char *, struct in_addr );
+static int joinleave6( int, int, const char *, struct in6_addr );
 
-static int joinleave( int Cmd, int UdpSock, const char *IfName, struct in_addr McAdr )
+int joinMcGroup4( int UdpSock, const char *IfName, struct in_addr McAdr )
+/*
+** Joins the MC group with the address 'McAdr' on the interface 'IfName'. 
+** The join is bound to the UDP socket 'UdpSock', so if this socket is 
+** closed the membership is dropped.
+**          
+** returns: - 0 if the function succeeds
+**          - 1 if parameters are wrong or the join fails
+*/
+{
+  return joinleave4( 'j', UdpSock, IfName, McAdr );
+}
+
+int leaveMcGroup4( int UdpSock, const char *IfName, struct in_addr McAdr )
+/*
+** Leaves the MC group with the address 'McAdr' on the interface 'IfName'. 
+**          
+** returns: - 0 if the function succeeds
+**          - 1 if parameters are wrong or the join fails
+*/
+{
+  return joinleave4( 'l', UdpSock, IfName, McAdr );
+}
+
+int joinMcGroup6( int UdpSock, const char *IfName, struct in6_addr McAdr )
+/*
+** Joins the MC group with the address 'McAdr' on the interface 'IfName'. 
+** The join is bound to the UDP socket 'UdpSock', so if this socket is 
+** closed the membership is dropped.
+**          
+** returns: - 0 if the function succeeds
+**          - 1 if parameters are wrong or the join fails
+*/
+{
+  return joinleave6( 'j', UdpSock, IfName, McAdr );
+}
+
+int leaveMcGroup6( int UdpSock, const char *IfName, struct in6_addr McAdr )
+/*
+** Leaves the MC group with the address 'McAdr' on the interface 'IfName'. 
+**          
+** returns: - 0 if the function succeeds
+**          - 1 if parameters are wrong or the join fails
+*/
+{
+  return joinleave6( 'l', UdpSock, IfName, McAdr );
+}
+
+static int joinleave4( int Cmd, int UdpSock, const char *IfName, struct in_addr McAdr )
 /*
 **          
 */
@@ -46,42 +96,54 @@ static int joinleave( int Cmd, int UdpSock, const char *IfName, struct in_addr M
   CtlReq.imr_interface.s_addr = IfDp->InAdr.s_addr;
 
   {
-    char FmtBu[ 32 ];
+    char FmtBu[ INET_ADDRSTRLEN ];
     smclog( LOG_NOTICE, 0, "%sMcGroup: %s on %s", CmdSt, 
-	 fmtInAdr( FmtBu, McAdr ), IfDp ? IfDp->Name : "<any>" );
+	    inet_ntop( AF_INET, &McAdr, FmtBu, sizeof( FmtBu ) ), IfDp ? IfDp->Name : "<any>" );
   }
 
   if( setsockopt( UdpSock, IPPROTO_IP, 
 		  Cmd == 'j' ? IP_ADD_MEMBERSHIP : IP_DROP_MEMBERSHIP, 
 		  (void *)&CtlReq, sizeof( CtlReq ) ) ) 
   {
-    smclog( LOG_WARNING, errno, "MRT_%s_MEMBERSHIP failed", Cmd == 'j' ? "ADD" : "DROP" );
+    smclog( LOG_WARNING, errno, "%s MEMBERSHIP failed", Cmd == 'j' ? "ADD" : "DROP" );
     return 1;
   }
 
   return 0;
 }
 
-int joinMcGroup( int UdpSock, const char *IfName, struct in_addr McAdr )
+static int joinleave6( int Cmd, int UdpSock, const char *IfName, struct in6_addr McAdr )
 /*
-** Joins the MC group with the address 'McAdr' on the interface 'IfName'. 
-** The join is bound to the UDP socket 'UdpSock', so if this socket is 
-** closed the membership is dropped.
 **          
-** returns: - 0 if the function succeeds
-**          - 1 if parameters are wrong or the join fails
 */
 {
-  return joinleave( 'j', UdpSock, IfName, McAdr );
+  struct ipv6_mreq CtlReq;
+  struct IfDesc *IfDp = getIfByName( IfName );
+  const char *CmdSt = Cmd == 'j' ? "join" : "leave";
+  
+
+  if( ! IfDp ) { 
+    smclog( LOG_WARNING, 0, "%sMcGroup, unknown interface %s", CmdSt, IfName );
+    return 1;
+  }
+
+  CtlReq.ipv6mr_multiaddr = McAdr;
+  CtlReq.ipv6mr_interface = IfDp->IfIndex;
+
+  {
+    char FmtBu[ INET6_ADDRSTRLEN ];
+    smclog( LOG_NOTICE, 0, "%sMcGroup: %s on %s", CmdSt, 
+	    inet_ntop( AF_INET6, &McAdr, FmtBu, sizeof( FmtBu ) ), IfDp ? IfDp->Name : "<any>" );
+  }
+
+  if( setsockopt( UdpSock, IPPROTO_IPV6, 
+		  Cmd == 'j' ? IPV6_JOIN_GROUP : IPV6_LEAVE_GROUP, 
+		  (void *)&CtlReq, sizeof( CtlReq ) ) ) 
+  {
+    smclog( LOG_WARNING, errno, "%s MEMBERSHIP failed", Cmd == 'j' ? "ADD" : "DROP" );
+    return 1;
+  }
+
+  return 0;
 }
 
-int leaveMcGroup( int UdpSock, const char *IfName, struct in_addr McAdr )
-/*
-** Leaves the MC group with the address 'McAdr' on the interface 'IfName'. 
-**          
-** returns: - 0 if the function succeeds
-**          - 1 if parameters are wrong or the join fails
-*/
-{
-  return joinleave( 'l', UdpSock, IfName, McAdr );
-}
