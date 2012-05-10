troglobit/smcroute

commit a15903c39c0c5c3fa4c79b850d8948988cc3d8a5

Cleanup, make code more readable, fix duplicate ifaces in list.

src/mroute-api.c
@@ -123,9 +123,13 @@ int mroute4_enable(void)
 	memset(&vif_list, 0, sizeof(vif_list));
 
 	/* create VIFs for all IP, non-loop interfaces */
-	for (i = 0; (iface = iface_find_by_index(i)); i++)
-		if (iface->inaddr.s_addr && !(iface->flags & IFF_LOOPBACK))
-			mroute4_add_vif(iface);
+	for (i = 0; (iface = iface_find_by_index(i)); i++) {
+		if (iface->flags & IFF_LOOPBACK) {
+			iface->vif = -1;
+			continue;
+		}
+		mroute4_add_vif(iface);
+	}
 
 	return 0;
 }
@@ -138,6 +142,7 @@ void mroute4_disable(void)
 	if (mroute4_socket < 0)
 		return;
 
+	/* Drop all kernel routes set by smcroute */
 	if (setsockopt(mroute4_socket, IPPROTO_IP, MRT_DONE, NULL, 0))
 		smclog(LOG_ERR, errno, "MRT_DONE");
 
@@ -151,8 +156,9 @@ void mroute4_disable(void)
 */
 static void mroute4_add_vif(struct iface *iface)
 {
-	struct vifctl VifCtl;
+	struct vifctl vc;
 	int vif = -1;
+	char buf[16];
 	size_t i;
 
 	/* search free vif */
@@ -169,19 +175,20 @@ static void mroute4_add_vif(struct iface *iface)
 		return;
 	}
 
-	memset(&VifCtl, 0, sizeof(VifCtl));
-	VifCtl.vifc_vifi = vif;
-	VifCtl.vifc_flags = 0;	        /* no tunnel, no source routing, register ? */
-	VifCtl.vifc_threshold = 1;	/* Packet TTL must be at least 1 to pass them */
-	VifCtl.vifc_rate_limit = 0;	/* hopefully no limit */
-	VifCtl.vifc_lcl_addr.s_addr = iface->inaddr.s_addr;
-	VifCtl.vifc_rmt_addr.s_addr = INADDR_ANY;
+	memset(&vc, 0, sizeof(vc));
+	vc.vifc_vifi = vif;
+	vc.vifc_flags = 0;      /* no tunnel, no source routing, register ? */
+	vc.vifc_threshold = 1;  /* Packet TTL must be at least 1 to pass them */
+	vc.vifc_rate_limit = 0;	/* hopefully no limit */
+	vc.vifc_lcl_addr.s_addr = iface->inaddr.s_addr;
+	vc.vifc_rmt_addr.s_addr = INADDR_ANY;
 
-	smclog(LOG_NOTICE, 0, "Add VIF: %d Flags: 0x%04x IP: 0x%08x Ifname: %s",
-	       VifCtl.vifc_vifi, VifCtl.vifc_flags, VifCtl.vifc_lcl_addr.s_addr,
+	smclog(LOG_NOTICE, 0, "Add VIF: %d Ifindex: %d Flags: 0x%04x IP: %s Ifname: %s",
+	       vc.vifc_vifi, iface->ifindex, vc.vifc_flags,
+	       inet_ntop (AF_INET, &vc.vifc_lcl_addr, buf, sizeof(buf)),
 	       iface->name);
 
-	if (setsockopt(mroute4_socket, IPPROTO_IP, MRT_ADD_VIF, (void *)&VifCtl, sizeof(VifCtl))) {
+	if (setsockopt(mroute4_socket, IPPROTO_IP, MRT_ADD_VIF, (void *)&vc, sizeof(vc))) {
 		smclog(LOG_ERR, errno, "MRT_ADD_VIF %s", iface->name);
 	} else {
 		iface->vif = vif;
@@ -195,7 +202,7 @@ static void mroute4_add_vif(struct iface *iface)
 ** returns: - 0 if the function succeeds
 **          - the errno value for non-fatal failure condition
 */
-int mroute4_add(struct mroute4 *ptr)
+int mroute4_add(mroute4_t *ptr)
 {
 	int result = 0;
 	char origin[INET_ADDRSTRLEN], group[INET_ADDRSTRLEN];
@@ -231,7 +238,7 @@ int mroute4_add(struct mroute4 *ptr)
 ** returns: - 0 if the function succeeds
 **          - the errno value for non-fatal failure condition
 */
-int mroute4_del(struct mroute4 *ptr)
+int mroute4_del(mroute4_t *ptr)
 {
 	int result = 0;
 	char origin[INET_ADDRSTRLEN], group[INET_ADDRSTRLEN];
@@ -331,9 +338,13 @@ int mroute6_enable(void)
 	}
 
 	/* create MIFs for all IP, non-loop interfaces */
-	for (i = 0; (iface = iface_find_by_index(i)); i++)
-		if (iface->inaddr.s_addr && !(iface->flags & IFF_LOOPBACK))
-			mroute6_add_mif(iface);
+	for (i = 0; (iface = iface_find_by_index(i)); i++) {
+		if (iface->flags & IFF_LOOPBACK) {
+			iface->vif = -1;
+			continue;
+		}
+		mroute6_add_mif(iface);
+	}
 
 	return 0;
 #endif /* HAVE_IPV6_MULTICAST_ROUTING */
@@ -392,10 +403,10 @@ static void mroute6_add_mif(struct iface *iface)
 	mc.vifc_rate_limit = 0;	/* hopefully no limit */
 #endif
 
-	smclog(LOG_NOTICE, 0, "Add MIF: %d Ifindex: %d Flags: 0x%x Ifname: %s",
+	smclog(LOG_NOTICE, 0, "Add MIF: %d Ifindex: %d Flags: 0x%04x Ifname: %s",
 	       mc.mif6c_mifi, mc.mif6c_pifi, mc.mif6c_flags, iface->name);
 
-	if (setsockopt(mroute6_socket, IPPROTO_IPV6, MRT6_ADD_MIF, (char *)&mc, sizeof(mc))) {
+	if (setsockopt(mroute6_socket, IPPROTO_IPV6, MRT6_ADD_MIF, (void *)&mc, sizeof(mc))) {
 		smclog(LOG_ERR, errno, "MRT6_ADD_MIF %s", iface->name);
 	} else {
 		iface->mif = mif;
@@ -409,7 +420,7 @@ static void mroute6_add_mif(struct iface *iface)
 ** returns: - 0 if the function succeeds
 **          - the errno value for non-fatal failure condition
 */
-int mroute6_add(struct mroute6 *ptr)
+int mroute6_add(mroute6_t *ptr)
 {
 	int result = 0;
 	size_t i;
@@ -447,7 +458,7 @@ int mroute6_add(struct mroute6 *ptr)
 ** returns: - 0 if the function succeeds
 **          - the errno value for non-fatal failure condition
 */
-int mroute6_del(struct mroute6 *ptr)
+int mroute6_del(mroute6_t *ptr)
 {
 	int result = 0;
 	char origin[INET_ADDRSTRLEN], group[INET_ADDRSTRLEN];

src/cmdpkt.c
@@ -39,17 +39,15 @@
 */
 void *cmd_build(char cmd, const char *argv[], int count)
 {
-	int num;
+	int i;
 	char *ptr;
 	const char **arg = argv;
 	unsigned arg_len = 0, packet_len;
 	struct cmd *packet;
 
 	/* Summarize length of all arguments/commands */
-	for (num = count; num; num--) {
-		arg_len += strlen(*arg) + 1;
-		arg++;
-	}
+	for (i = 0; i < count; i++)
+		arg_len += strlen(argv[i]) + 1;
 
 	/* resulting packet size */
 	packet_len = sizeof(struct cmd) + arg_len + 1;
@@ -65,13 +63,11 @@ void *cmd_build(char cmd, const char *argv[], int count)
 	packet->count = count;
 
 	/* copy args */
-	ptr = (char *)(packet + 1);
-	arg = argv;
-	for (num = count; num; num--) {
-		arg_len = strlen(*arg) + 1;
-		memcpy(ptr, *arg, arg_len);
+	ptr = (char *)(packet->argv);
+	for (i = 0; i < count; i++) {
+		arg_len = strlen(argv[i]) + 1;
+		memcpy(ptr, argv[i], arg_len);
 		ptr += arg_len;
-		arg++;
 	}
 	*ptr = '\0';	/* '\0' behind last string */
 
@@ -88,7 +84,7 @@ void *cmd_build(char cmd, const char *argv[], int count)
 */
 const char *cmd_convert_to_mroute(struct mroute *mroute, const struct cmd *packet)
 {
-	char *arg = (char *)(packet + 1);
+	char *arg = (char *)packet->argv;
 
 	memset(mroute, 0, sizeof(*mroute));
 
@@ -144,7 +140,7 @@ const char *cmd_convert_to_mroute(struct mroute *mroute, const struct cmd *packe
 */
 const char *cmd_convert_to_mroute4(struct mroute4 *mroute, const struct cmd *packet)
 {
-	const char *arg = (const char *)(packet + 1);
+	char *arg = (char *)packet->argv;
 
 	memset(mroute, 0, sizeof(*mroute));
 

src/ifvc.c
@@ -58,14 +58,17 @@ void iface_init(void)
 		if (ifa->ifa_addr == NULL)
 			continue;
 
-		iface  = &iface_list[num_ifaces++];
-		family = ifa->ifa_addr->sa_family;
-
 		/* Skip non-IPv4 and non-IPv6 interfaces */
+		family = ifa->ifa_addr->sa_family;
 		if ((family != AF_INET) && (family != AF_INET6))
 			continue;
 
+		/* Check if already added? */
+		if (iface_find_by_name (ifa->ifa_name))
+			continue;
+
 		/* Copy data from interface iterator 'ifa' */
+		iface = &iface_list[num_ifaces++];
 		strncpy(iface->name, ifa->ifa_name, sizeof(iface->name));
 		if (family == AF_INET)
 			iface->inaddr.s_addr = ((struct sockaddr_in *)ifa->ifa_addr)->sin_addr.s_addr;

src/ipc.c
@@ -128,19 +128,24 @@ struct cmd *ipc_server_read(uint8 buf[], int len)
 			smclog(LOG_DEBUG, 0, "%s: waiting for connection...", __FUNCTION__);
 
 			if ((client_sd = accept(server_sd, NULL, &socklen)) < 0)
-				smclog(LOG_ERR, errno, "%s: accept() failed", __FUNCTION__);
+				smclog(LOG_ERR, errno, "%s: accept() failed incoming connection", __FUNCTION__);
 
-			smclog(LOG_DEBUG, 0, "%s: accepted connection", __FUNCTION__);
+			smclog(LOG_DEBUG, 0, "%s: connection accepted", __FUNCTION__);
 		}
 
 		/* read */
 		memset(buf, 0, len);	/* had some problems with buffer garbage */
 		size = read(client_sd, buf, len);
-		smclog(LOG_DEBUG, 0, "%s: command read (%zu)", __FUNCTION__, size);
+		smclog(LOG_DEBUG, 0, "%s: command read(%d, %p, %d) => %zu should be at least %zu", __FUNCTION__,
+		       client_sd, buf, len, size, sizeof(struct cmd));
 
-		/* successfull read */
-		if (size >= sizeof(struct cmd) && size == ((struct cmd *)buf)->len)
-			return (struct cmd *)buf;
+		/* successful read */
+		if (size >= sizeof(struct cmd)) {
+			struct cmd *p = (struct cmd *)buf;
+
+			if (size == p->len)
+				return p;
+		}
 
 		/* connection lost ? -> reset connection */
 		if (!size) {

src/mclab.h
@@ -124,6 +124,7 @@ struct mroute4 {
 	short inbound;                  /* incoming VIF    */
 	uint8 ttl[MAX_MC_VIFS];         /* outgoing VIFs   */
 };
+typedef struct mroute4 mroute4_t;
 
 /*
  * IPv6 multicast route
@@ -134,6 +135,7 @@ struct mroute6 {
 	short inbound;                  /* incoming VIF    */
 	uint8 ttl[MAX_MC_MIFS];         /* outgoing VIFs   */
 };
+typedef struct mroute6 mroute6_t;
 
 /*
  * Generic multicast route (wrapper for IPv4/IPv6 mroute) 
@@ -141,10 +143,11 @@ struct mroute6 {
 struct mroute {
 	int version;		/* 4 or 6 */
 	union {
-		struct mroute4 mroute4;
-		struct mroute6 mroute6;
+		mroute4_t mroute4;
+		mroute6_t mroute6;
 	} u;
 };
+typedef struct mroute mroute_t;
 
 /* 
  * Raw IGMP socket used as interface for the IPv4 mrouted API.
@@ -160,13 +163,13 @@ extern int mroute6_socket;
 
 int  mroute4_enable  (void);
 void mroute4_disable (void);
-int  mroute4_add     (struct mroute4 *mroute);
-int  mroute4_del     (struct mroute4 *mroute);
+int  mroute4_add     (mroute4_t *mroute);
+int  mroute4_del     (mroute4_t *mroute);
 
 int  mroute6_enable  (void);
 void mroute6_disable (void);
-int  mroute6_add     (struct mroute6 *mroute);
-int  mroute6_del     (struct mroute6 *mroute);
+int  mroute6_add     (mroute6_t *mroute);
+int  mroute6_del     (mroute6_t *mroute);
 
 /* ipc.c */
 int         ipc_server_init (void);
@@ -181,18 +184,18 @@ void        ipc_exit        (void);
  * XXX: Add example packet layouts
  */
 struct cmd {
-	unsigned len;		/* total size of packet including cmd header */
+	size_t   len;		/* total size of packet including cmd header */
 	uint16   cmd;		/* 'a'=Add,'r'=Remove,'j'=Join,'l'=Leave,'k'=Kill */
 	uint16   count;		/* command argument count */
-	/* 'count' * '\0' terminated strings + '\0' */
+	char    *argv[0]; 	/* 'count' * '\0' terminated strings + '\0' */
 };
 
 #define MX_CMDPKT_SZ 1024	/* command size including appended strings */
 
 void       *cmd_build              (char cmd, const char *argv[], int count);
-const char *cmd_convert_to_mroute  (struct mroute  *mroute, const struct cmd *packet);
-const char *cmd_convert_to_mroute4 (struct mroute4 *mroute, const struct cmd *packet);
-const char *cmd_convert_to_mroute6 (struct mroute6 *mroute, const struct cmd *packet);
+const char *cmd_convert_to_mroute  (mroute_t *mroute, const struct cmd *packet);
+const char *cmd_convert_to_mroute4 (mroute4_t *mroute, const struct cmd *packet);
+const char *cmd_convert_to_mroute6 (mroute6_t *mroute, const struct cmd *packet);
 
 /* mcgroup.c */
 int mcgroup4_join  (const char *ifname, struct in_addr  group);

src/parse-conf.c
@@ -114,7 +114,7 @@ static int add_mroute (int lineno, char *ifname, char *group, char *source, char
 		smclog(LOG_WARNING, 0, "Line %02: Sadly this build of smcroute does not support IPv6.", lineno);
 		result = 0;
 #else
-		struct mroute6 mroute;
+		mroute6_t mroute;
 
 		mroute.inbound = iface_get_mif_by_name(ifname);
 		if (mroute.inbound < 0) {

src/smcroute.c
@@ -566,6 +566,7 @@ int main(int argc, const char *argv[])
 			int slen = 0, rlen = 0;
 			struct cmd *command = cmdv[i];
 
+			smclog(LOG_DEBUG, 0, "Sending command %c len:%zu count:%d", command->cmd, command->len, command->count);
 			slen = ipc_send(command, command->len);
 			rlen = ipc_receive(buf, sizeof(buf));
 			if (slen < 0 || rlen < 0)

commit 20cb3fc53d518f6a0048020f3a800173f4782cab

Bugfix: Regression in a15903c39 - don't add VIFs for interfaces w/o a valid address.

src/mroute-api.c
@@ -131,9 +131,11 @@ int mroute4_enable(void)
 	/* Initialize virtual interface table */
 	memset(&vif_list, 0, sizeof(vif_list));
 
-	/* create VIFs for all IP, non-loop interfaces */
+	/* Create virtual interfaces, VIFs, for all non-loopback interfaces
+	 * that have a valid IPv4 address. */
 	for (i = 0; (iface = iface_find_by_index(i)); i++) {
-		if (iface->flags & IFF_LOOPBACK) {
+		if (iface->flags & IFF_LOOPBACK ||
+		    iface->inaddr.s_addr == INADDR_ANY) {
 			iface->vif = -1;
 			continue;
 		}