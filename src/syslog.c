88e307ad90c4e5f15448712312c94045f3b22e55
Joachim Nilsson <troglobit@gmail.com>
2013-08-17 15:44:22 +0200

    Greatly simplify smclog()
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/syslog.c
+++ src/syslog.c
@@ -28,54 +28,32 @@ int  log_last_severity;
 int  log_last_error;
 char log_last_message[128];
 
-/*
- * Writes the message 'fmt' with the parameters '...' to syslog.
- * 'severity' is used for the syslog entry. For an 'code' value
- * other than 0, the correponding error string is appended to the
- * message.
+/**
+ * smclog - Log message to syslog and stderr
+ * @severity: Standard syslog() severity levels or %LOG_INIT
+ * @code:     Error code, @errno, or zero if unused
+ * @fmt:      Standard printf() formatted message to log
+ * 
+ * Logs a standard printf() formatted message to syslog and stderr when
+ * @severity is greater than the @log_stderr threshold.  When @code is
+ * set it is appended to the log, along with the error message.
  *
- * For a 'severity' more important than 'LOG_WARNING' the message is
- * also logged to 'stderr' and the program is finished with a call to
- * 'exit()'.
- *
- * If the 'severity' is more important than 'log_stderr' the message
- * is logged to 'stderr'.
+ * When @severity is %LOG_ERR or worse this function will call exit().
  */
 void smclog(int severity, int code, const char *fmt, ...)
 {
-	int arg_len;
+	int len;
 	va_list args;
-	const char severity_list[][5] = {
-		"EMER", "ALER", "CRIT", "ERRO",
-		"Warn", "Note", "Info", "Debu"
-	};
-	const char *severity_string;
-	const char *error_string = (code <= 0) ? NULL : (const char *)strerror(code);
-
-	/* LOG_INIT is a gross hack to work around smcroute's bad architecture
-	 * During daemon init, we do not want to trigger the exit() call at the end
-	 * of the function to be able to return an exit code from the parent before
-	 * we daemonize, without the parent triggering the atexit() handlers in the
-	 * normal case (which would remove the socket...)
-	 * That gross, ugly hack or a complete rewrite, for now the hack will do. */
-	if (severity < 0 || severity >= (int)ARRAY_ELEMENTS(severity_list)) {
-		if (severity == LOG_INIT)
-			severity_string = "INIT";
-		else
-			severity_string = "!unknown serverity!";
-	} else {
-		severity_string = severity_list[severity];
-	}
+	const char *err = (code <= 0) ? NULL : (const char *)strerror(code);
 
 	/* Skip logging for severities 'DEBUG' if do_debug_logging is false */
 	if (severity == LOG_DEBUG && !do_debug_logging)
 		return;
 
 	va_start(args, fmt);
-	arg_len  = snprintf(log_last_message, sizeof(log_last_message), "%s: ", severity_string);
-	arg_len += vsnprintf(log_last_message + arg_len, sizeof(log_last_message) - arg_len, fmt, args);
-	if (error_string)
-		snprintf(log_last_message + arg_len, sizeof(log_last_message) - arg_len, ". Error %d: %s", code, error_string);
+	len += vsnprintf(log_last_message + len, sizeof(log_last_message) - len, fmt, args);
+	if (err)
+		snprintf(log_last_message + len, sizeof(log_last_message) - len, ". Error %d: %s", code, err);
 	va_end(args);
 
 	/* update our global Last... variables */
@@ -83,7 +61,7 @@ void smclog(int severity, int code, const char *fmt, ...)
 	log_last_error = code;
 
 	/* control logging to stderr */
-	if (severity < LOG_WARNING || severity < log_stderr || severity == LOG_INIT)
+	if (severity < log_stderr || severity == LOG_INIT)
 		fprintf(stderr, "%s\n", log_last_message);
 
 	/* always to syslog */
