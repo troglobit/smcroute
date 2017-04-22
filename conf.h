b2b571cf1dab9906c93d512a44012e87c46e6ffe
Joachim Nilsson <troglobit@gmail.com>
2017-04-22 12:53:33 +0200

    Followup to 578cd4b, actually add new conf.h
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ conf.h
@@ -0,0 +1,17 @@
+#ifndef SMCROUTE_CONF_H_
+#define SMCROUTE_CONF_H_
+
+#include "mclab.h"
+
+int run_script(struct mroute *mroute);
+int parse_conf_file(const char *file);
+void read_conf_file(const char *file);
+
+#endif /* SMCROUTE_CONF_H_ */
+
+/**
+ * Local Variables:
+ *  indent-tabs-mode: t
+ *  c-file-style: "linux"
+ * End:
+ */
