3a007a3fefffa578872cbfbdd22f04e7c7ebed06
Joachim Nilsson <troglobit@gmail.com>
2017-04-23 22:24:52 +0200

    Followup to d9afcb8, add missing mcgroup.h
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ mcgroup.h
@@ -0,0 +1,20 @@
+/* IGMP/MLD group subscription API */
+#ifndef SMCROUTE_MCGROUP_H_
+#define SMCROUTE_MCGROUP_H_
+
+int  mcgroup4_join      (const char *ifname, struct in_addr  source, struct in_addr  group);
+int  mcgroup4_leave     (const char *ifname, struct in_addr  source, struct in_addr  group);
+void mcgroup4_disable   (void);
+
+int  mcgroup6_join      (const char *ifname, struct in6_addr group);
+int  mcgroup6_leave     (const char *ifname, struct in6_addr group);
+void mcgroup6_disable   (void);
+
+#endif /* SMCROUTE_MCGROUP_H_ */
+
+/**
+ * Local Variables:
+ *  indent-tabs-mode: t
+ *  c-file-style: "linux"
+ * End:
+ */
