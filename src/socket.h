da4ef992d5e74061fafdbcffaed155053a5718fa
Joachim Nilsson <troglobit@gmail.com>
2017-05-20 15:48:22 +0200

    Fix include path for time.h, struct timeval is in sys/time.h
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/socket.h
+++ src/socket.h
@@ -22,7 +22,7 @@
 
 #include <stdarg.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 
 int socket_register(int sd, void (*cb)(int, void *), void *arg);
 int socket_create  (int domain, int type, int proto, void (*cb)(int, void *), void *arg);
