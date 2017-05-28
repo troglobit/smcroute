1f89bba4a371db7be8de2f64539da1de3037dea6
Joachim Nilsson <troglobit@gmail.com>
2017-05-28 23:32:21 +0200

    Fix build error on FreeBSD 9-RELEASE
    To use getifaddrs() on older FreeBSD releases one needs to include
    sys/socket.h *before* ifaddrs.h, otherwise struct sockaddr is not
    defined.  This is fixed in FreeBSD 10 and later.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/ifvc.c
+++ src/ifvc.c
@@ -24,9 +24,10 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <ifaddrs.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 
 #include "log.h"
