d8321140bf84ceb37b58314202e32455ac6828bb
Joachim Nilsson <troglobit@gmail.com>
2017-05-30 22:09:04 +0200

    Move utimensat() replacement to use AC_REPLACE_FUNCS() framework
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/util.h
+++ src/util.h
@@ -1,7 +1,8 @@
-/* Utilitity functions */
+/* Utilitity and replacement functions */
 #ifndef SMCROUTE_UTIL_H_
 #define SMCROUTE_UTIL_H_
 
+#include "config.h"
 #include <string.h>
 #include "mroute.h"
 
@@ -18,8 +19,9 @@
 #endif
 
 int pidfile(const char *basename, uid_t uid, gid_t gid);
+#ifndef HAVE_UTIMENSAT
 int utimensat(int dirfd, const char *pathname, const struct timespec ts[2], int flags);
-
+#endif
 #ifndef HAVE_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t len);
 #endif
