4668646dac4578eac630cddb76768566be9df3db
Joachim Nilsson <troglobit@gmail.com>
2017-06-28 09:38:22 +0200

    An unsigned long can be 10 digits, increase fmt specifiers
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/smcroutectl.c
+++ src/smcroutectl.c
@@ -138,7 +138,7 @@ static int get_width(void)
 static void table_heading(char *argv[], size_t count, int detail)
 {
 	int len;
-	char line[85];
+	char line[90];
 	const char *g = "GROUP (S,G)", *i = "INBOUND";
 	const char *r = "ROUTE (S,G)", *o = "OUTBOUND", *p = "PACKETS", *b = "BYTES";
 
@@ -149,7 +149,7 @@ static void table_heading(char *argv[], size_t count, int detail)
 	if (count && argv[0][0] == 'g')
 		snprintf(line, sizeof(line), "\e[7m%-34s %-16s", g, i);
 	else if (detail)
-		snprintf(line, sizeof(line), "\e[7m%-34s %-16s %7s %8s  %-8s", r, i, p, b, o);
+		snprintf(line, sizeof(line), "\e[7m%-34s %-16s %10s %10s %-8s", r, i, p, b, o);
 	else
 		snprintf(line, sizeof(line), "\e[7m%-34s %-16s %-8s", r, i, o);
 
