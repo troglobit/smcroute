8b9d623c20ee5fa75dd55ace86f8326b08fe3056
Joachim Wiberg <troglobit@gmail.com>
2021-07-13 11:13:54 +0200

    test: fix buggy basic test and add some kernel state debug
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/basic.sh
+++ test/basic.sh
@@ -17,7 +17,7 @@ cat <<EOF > basic.conf
 # basic (*,G) multicast routing
 phyint a1 enable
 phyint a2 enable
-mroute from a1 source 10.0.0.10 group 225.3.2.1 to a2
+mroute from a1 source 10.0.0.1 group 225.3.2.1 to a2
 mroute from a1 group 225.1.2.3 to a2
 EOF
 cat basic.conf
@@ -27,12 +27,16 @@ echo "Starting smcrouted ..."
 sleep 1
 
 echo "Starting collector ..."
-tcpdump -c 5 -lni a2 -w basic.pcap icmp and dst 225.1.2.3 &
+tcpdump -c 5 -lni a2 -w basic.pcap 'dst 225.3.2.1 or dst 225.1.2.3' &
 sleep 1
 
 echo "Starting emitter ..."
 ping -c 3 -W 1 -I a1 -t 2 225.3.2.1
 ping -c 3 -W 1 -I a1 -t 2 225.1.2.3
+
+# Show active routes (and counters)
+cat /proc/net/ip_mr_cache
+ip mroute
 
 echo "Cleaning up ..."
 killall smcrouted
@@ -49,5 +53,5 @@ echo "Routed frames for group 225.3.2.1 => $lines2"
 
 # one frame lost due to initial (*,G) -> (S,G) route setup
 # no frames lost in pure (S,G) route
-[ "$lines1" != "2" || "$lines2" != "3" ] && exit 1
-exit 0
+[ "$lines1" = "2" -a "$lines2" = "3" ] && exit 0
+exit 1
