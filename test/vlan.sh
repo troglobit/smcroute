0b2bffb7928f96875e9f1cd587478b1436d849f3
Joachim Wiberg <troglobit@gmail.com>
2021-07-13 11:15:56 +0200

    test: update test description and add some kernel state debug
    Use same logic for returning test result as basic.sh, this prevents
    any accidental false positive.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/vlan.sh
+++ test/vlan.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Verify IPv4 (*,G) routing on top of VLAN interfaces
 set -x
 
 echo "Creating world ..."
@@ -37,6 +38,10 @@ sleep 1
 echo "Starting emitter ..."
 ping -c 3 -W 1 -I a1.110 -t 2 225.1.2.3
 
+# Show active routes (and counters)
+cat /proc/net/ip_mr_cache
+ip mroute
+
 echo "Cleaning up ..."
 killall smcrouted
 sleep 1
@@ -46,8 +51,8 @@ ip link del a2
 echo "Analyzing ..."
 lines=$(tcpdump -r vlan.pcap | grep 225.1.2.3 | tee vlan.result | wc -l)
 cat vlan.result
-echo "=> num routes frames $lines"
+echo "Routed frames for group 225.1.2.3 => $lines"
 
 # one frame lost due to initial (*,G) -> (S,G) setup
-[ "$lines" != "2" ] && exit 1
-exit 0
+[ "$lines" = "2" ] && exit 0
+exit 1
