39d5bf3e9bddde77f644333e524d3bc0879d471a
Joachim Wiberg <troglobit@gmail.com>
2021-07-27 18:20:04 +0200

    test: convert bridge test to use new isolated bridge topo
    Last test to require nemesis.  We can now drop the requirement to run
    tests in our custom docker \o/
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/bridge.sh
+++ test/bridge.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 # Verifies (*,G) routing between VLANs on top of a VLAN filtering bridge
-# with bridge ports as VETH interfaces.  UDP injection with nemesis, see
-# test/README.md as for why.
+# with bridge ports as VETH interfaces.  The endpoints of the VETH pairs
+# are placed in isolated network namespaces to allow IP networking as
+# one expects.
 #set -x
 
 # shellcheck source=/dev/null
 . "$(dirname "$0")/lib.sh"
 
+LEFT=/tmp/$NM/b1
+RIGHT=/tmp/$NM/b2
+
 print "Creating world ..."
-topo bridge
+topo isolated bridge "$LEFT" "$RIGHT"
+
+# IP World
 ip addr add 10.0.0.1/24 dev vlan1
+nsenter --net="$LEFT"  -- ip addr add 10.0.0.10/24 dev eth0
+
 ip addr add 20.0.0.1/24 dev vlan2
+nsenter --net="$RIGHT" -- ip addr add 20.0.0.10/24 dev eth0
+bridge link
+bridge vlan
 ip -br a
 
 print "Creating config ..."
@@ -27,11 +38,11 @@ print "Starting smcrouted ..."
 sleep 1
 
 print "Starting collector ..."
-tshark -c 2 -lni a2 -w "/tmp/$NM/pcap" dst 225.1.2.3 2>/dev/null &
+nsenter --net="$RIGHT" -- tshark -c 2 -lni eth0 -w "/tmp/$NM/pcap" 'dst 225.1.2.3' 2>/dev/null &
 sleep 1
 
 print "Starting emitter ..."
-nemesis udp -c 3 -S 10.0.0.10 -D 225.1.2.3 -T 3 -M 01:00:5e:01:02:03 -d a1
+nsenter --net="$LEFT" -- ping -c 3 -W 1 -I eth0 -t 3 225.1.2.3
 show_mroute
 
 print "Analyzing ..."
