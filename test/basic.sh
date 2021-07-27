4153774aef285f5426ec62b118e2457f0400d0af
Joachim Wiberg <troglobit@gmail.com>
2021-07-27 10:18:49 +0200

    test: update basic.sh wih an (S,G) route added over IPC
    This test now covers most of the basic functionality:
    
      - IPv4 (S,G) added from file
      - IPv4 (*,G) added from file
      - IPv4 (S,G) added over IPC
      - IPv4 (*,G) added over IPC
    
    Remaining: IPv6 and remove with negative testing.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/basic.sh
+++ test/basic.sh
@@ -27,24 +27,28 @@ print "Starting smcrouted ..."
 sleep 1
 
 print "Starting collector ..."
-tshark -Qc 7 -lni a2 -w "/tmp/$NM/pcap" 'dst 225.3.2.1 or dst 225.1.2.3 or dst 225.1.2.4' 2>/dev/null &
+tshark -Qc 10 -lni a2 -w "/tmp/$NM/pcap" 'dst 225.3.2.1 or dst 225.1.2.3 or dst 225.1.2.4 or 225.1.2.5' 2>/dev/null &
 
 ../src/smcroutectl -S "/tmp/$NM/sock" add a1 225.1.2.4 a2
+../src/smcroutectl -S "/tmp/$NM/sock" add a1 10.0.0.1 225.1.2.5 a2
 
 print "Starting emitter ..."
 ping -c 3 -W 1 -I a1 -t 2 225.3.2.1 >/dev/null
 ping -c 3 -W 1 -I a1 -t 2 225.1.2.3 >/dev/null
 ping -c 3 -W 1 -I a1 -t 2 225.1.2.4 >/dev/null
+ping -c 3 -W 1 -I a1 -t 2 225.1.2.5 >/dev/null
 show_mroute
 
 print "Analyzing ..."
 lines1=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.1.2.3 | tee    "/tmp/$NM/result" | wc -l)
 lines2=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.3.2.1 | tee -a "/tmp/$NM/result" | wc -l)
 lines3=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.1.2.4 | tee -a "/tmp/$NM/result" | wc -l)
+lines4=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.1.2.5 | tee -a "/tmp/$NM/result" | wc -l)
 cat "/tmp/$NM/result"
 echo " => $lines1 for 225.1.2.3, expected => 2"
 echo " => $lines2 for 225.3.2.1, expected => 3"
 echo " => $lines3 for 225.1.2.4, expected => 2"
+echo " => $lines4 for 225.1.2.5, expected => 3"
 
 print "Cleaning up ..."
 topo teardown
@@ -52,5 +56,5 @@ topo teardown
 # one frame lost due to initial (*,G) -> (S,G) route setup
 # no frames lost in pure (S,G) route
 # shellcheck disable=SC2166
-[ "$lines1" = "2" -a "$lines2" = "3" -a "$lines3" = "2" ] && exit 0
+[ "$lines1" = "2" -a "$lines2" = "3" -a "$lines3" = "2" -a "$lines4" = "3" ] && exit 0
 exit 1
