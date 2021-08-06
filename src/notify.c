3a6f96ad394ab6fb70d91ac33cf18ee0734a2021
Joachim Wiberg <troglobit@gmail.com>
2021-08-06 08:32:48 +0200

    Minor, add file license header and Emacs trailer hints
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/notify.c
+++ src/notify.c
@@ -1,4 +1,21 @@
-/* generic service monitor backend */
+/* generic service monitor backend
+ *
+ * Copyright (C) 2019-2021  Joachim Wiberg <troglobit@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
 
 #include <errno.h>
 #include <unistd.h>
@@ -25,3 +42,10 @@ void notify_reload(void)
 	systemd_notify_reload(msg);
 	smclog(LOG_NOTICE, msg);
 }
+
+/**
+ * Local Variables:
+ *  indent-tabs-mode: t
+ *  c-file-style: "linux"
+ * End:
+ */
