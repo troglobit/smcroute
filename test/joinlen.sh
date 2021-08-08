e28b2eaf11dd7fb5915b2a28f231613e32d8cc3f
Joachim Wiberg <troglobit@gmail.com>
2021-08-08 12:50:59 +0200

    test: refactor joinlen.sh and add S/LEN verification
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/joinlen.sh
+++ test/joinlen.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# only checking for a sample in each range
+check_output()
+{
+    print "Verifying ..."
+    if [ -n "$1" ]; then
+	ip maddr show dev a2
+	if ! ip maddr show dev a2 | grep -q "$1"; then
+	    FAIL "Cannot find (* $1)"
+	fi
+    fi
+
+    if [ -n "$2" ]; then
+	cat /proc/net/mcfilter
+	if ! grep -q "$2" /proc/net/mcfilter; then
+	    FAIL "Cannot find ($2)"
+	fi
+    fi
+}
+
 # shellcheck source=/dev/null
 . "$(dirname "$0")/lib.sh"
 
@@ -29,38 +48,32 @@ print "Starting smcrouted ..."
 ../src/smcrouted -f "/tmp/$NM/conf" -n -N -P "/tmp/$NM/pid" -l debug -S "/tmp/$NM/sock" &
 sleep 1
 
-cat /proc/net/mcfilter
-ip maddr show dev a2
-
-print "Verifying ..."
-# only checking for a sample in each range
-ip maddr show dev a2 | grep 225.3.2.249
-config_asm=$?
-
-grep "0xe101022a 0x0a00000a" /proc/net/mcfilter
-config_ssm=$?
+check_output "225.3.2.249" "0xe101022a 0x0a00000a"
 
 ################################################################### LEAVE GROUPS
 ../src/smcroutectl -S "/tmp/$NM/sock" leave a1 10.0.0.10 225.1.2.40/24
 ../src/smcroutectl -S "/tmp/$NM/sock" leave a2 225.3.2.250/24
+cat /proc/net/mcfilter
+ip maddr show dev a2
 
 #################################################################### JOIN GROUPS
 print "Phase 2: Join groups (IPC)"
 ../src/smcroutectl -S "/tmp/$NM/sock" join a1 10.0.0.10 225.1.1.1/30
 ../src/smcroutectl -S "/tmp/$NM/sock" join a2 225.0.0.1/30
+check_output "225.0.0.2" "0xe1010102 0x0a00000a"
 
-cat /proc/net/mcfilter
-ip maddr show dev a2
+################################################################### LEAVE GROUPS
+print "Debug 1 ..."
+../src/smcroutectl -S "/tmp/$NM/sock" show group
+../src/smcroutectl -S "/tmp/$NM/sock" leave a1 10.0.0.10 225.1.1.1/30
+../src/smcroutectl -S "/tmp/$NM/sock" leave a2 225.0.0.1/30
 
-print "Verifying ..."
-# only checking for a sample in each range
-ip maddr show dev a2 | grep 225.0.0.2
-join_asm=$?
-
-grep "0xe1010102 0x0a00000a" /proc/net/mcfilter
-join_ssm=$?
+################################################################### JOIN SOURCES
+print "Debug 2 ..."
+../src/smcroutectl -S "/tmp/$NM/sock" show group
+print "Phase 3: Join group from multiple sources (IPC)"
+../src/smcroutectl -S "/tmp/$NM/sock" join a1 10.0.0.1/26 225.1.2.3
+check_output "" "0xe1010203 0x0a00003e"
 
 ########################################################################### DONE
-# shellcheck disable=SC2166
-[ $config_asm -eq 0 -a $config_ssm -eq 0 -a $join_asm -eq 0 -a $join_ssm -eq 0 ] && OK
-FAIL
+OK
