48d049e03e41b3f46e668b818b8e9b9977caad6c
Joachim Wiberg <troglobit@gmail.com>
2021-08-09 10:54:45 +0200

    test: dyn.sh: also verify (S,G/LEN), which also works now
    Everything with a non-fixed prefix length (32 or 128) will be treated as
    a dynamic route and only be installed when a matching sender appears.
    
    Fixes #135

--- test/dyn.sh
+++ test/dyn.sh
@@ -26,6 +26,8 @@ mroute from a1 group ff2e::42/121 to a2
 
 mroute from a1 source 10.0.0.1/24 group 225.3.2.1 to a2
 mroute from a1 source fc00::1/64  group ff04::114 to a2
+
+mroute from a1 source 10.0.0.1 group 225.3.3.3/24 to a2
 EOF
 cat "/tmp/$NM/conf"
 
@@ -38,7 +40,7 @@ echo "--------------------------------------------------------------------------
 echo "-----------------------------------------------------------------------------------"
 ../src/smcroutectl -pS "/tmp/$NM/sock" show routes
 
-collect a2 -c18 'dst 225.1.2.1 or dst 225.1.2.201 or dst ff2e::42 or dst ff2e::7f or dst 225.3.2.1 or dst ff04::114'
+collect a2 -c21 'dst 225.1.2.1 or dst 225.1.2.201 or dst ff2e::42 or dst ff2e::7f or dst 225.3.2.1 or dst ff04::114 or dst 225.3.3.1'
 
 print "Starting emitter ..."
 ping    -c 3 -W 1 -I a1       -t 2 225.1.2.1   >/dev/null
@@ -47,6 +49,7 @@ ping -6 -c 3 -W 1 -I fc00::42 -t 2 ff2e::42    >/dev/null
 ping -6 -c 3 -W 1 -I fc00::42 -t 2 ff2e::7f    >/dev/null
 ping    -c 3 -W 1 -I a1       -t 2 225.3.2.1   >/dev/null
 ping -6 -c 3 -W 1 -I fc00::42 -t 2 ff04::114   >/dev/null
+ping    -c 3 -W 1 -I a1       -t 2 225.3.3.1   >/dev/null
 show_mroute
 
 print "Analyzing ..."
@@ -56,6 +59,7 @@ lines3=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep ff2e::42    | tee "/tmp/$N
 lines4=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep ff2e::7f    | tee "/tmp/$NM/result" | wc -l)
 lines5=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.3.2.1   | tee "/tmp/$NM/result" | wc -l)
 lines6=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep ff04::114   | tee "/tmp/$NM/result" | wc -l)
+lines7=$(tshark -r "/tmp/$NM/pcap" 2>/dev/null | grep 225.3.3.1   | tee "/tmp/$NM/result" | wc -l)
 cat "/tmp/$NM/result"
 echo " => $lines1 for 225.1.2.1 ,  expected >= 2"
 echo " => $lines2 for 225.1.2.201, expected >= 2"
@@ -63,9 +67,10 @@ echo " => $lines3 for ff2e::42,    expected >= 2"
 echo " => $lines4 for ff2e::7f,    expected >= 2"
 echo " => $lines5 for 225.3.2.1,   expected >= 2"
 echo " => $lines6 for ff04::114,   expected >= 2"
+echo " => $lines7 for 225.3.3.1,   expected >= 2"
 
 ########################################################################### DONE
 # Expect one frame lost due to initial (*,G) -> (S,G) route setup
 # shellcheck disable=SC2166 disable=SC2086
-[ $lines1 -ge 2 -a $lines2 -ge 2 -a $lines3 -ge 2 -a $lines4 -ge 2 -a $lines5 -ge 2 -a $lines6 -ge 2 ] && OK
+[ $lines1 -ge 2 -a $lines2 -ge 2 -a $lines3 -ge 2 -a $lines4 -ge 2 -a $lines5 -ge 2 -a $lines6 -ge 2 -a $lines7 -ge 2 ] && OK
 FAIL
