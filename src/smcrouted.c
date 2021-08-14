troglobit/smcroute

commit 575ccf2d63219bb98660b7076171e442e60b769c

Fix minor memory leak in compose_paths() found by valgrind
Not critical, only allocated once at startup.

Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/smcrouted.c
@@ -215,6 +215,16 @@ static int start_server(void)
 	return server_loop();
 }
 
+static void cleanup(void)
+{
+	if (conf_file)
+		free(conf_file);
+	conf_file = NULL;
+	if (sock_file)
+		free(sock_file);
+	sock_file = NULL;
+}
+
 static int compose_paths(void)
 {
 	/* Default .conf file path: "/etc" + '/' + "smcroute" + ".conf" */
@@ -246,12 +256,14 @@ static int compose_paths(void)
 	if (!pid_file)
 		pid_file = ident;
 
+	atexit(cleanup);
+
 	return 0;
 }
 
 static int usage(int code)
 {
-        char pidfn[80];
+        char pidfn[sizeof(RUNSTATEDIR) + strlen(pid_file) + 6];
 
 	compose_paths();
 	if (pid_file[0] != '/')
@@ -293,6 +305,8 @@ static int usage(int code)
 	       "  -t ID           Set multicast routing table ID, default: 0\n"
 	       "  -v              Show program version\n"
 	       "\n", prognm, conf_file, ident, pidfn, sock_file);
+
+	cleanup();
 
 	return code;
 }
@@ -346,7 +360,7 @@ int main(int argc, char *argv[])
 			conf_vrfy = 1;
 			/* fallthrough */
 		case 'f':
-			conf_file = optarg;
+			conf_file = strdup(optarg);
 			break;
 
 		case 'h':	/* help */
@@ -384,6 +398,7 @@ int main(int argc, char *argv[])
 			break;
 
 		case 'P':
+			/* Handled separately from the other files, no strdup() */
 			pid_file = optarg;
 			break;
 
@@ -392,7 +407,7 @@ int main(int argc, char *argv[])
 			break;
 
 		case 'S':
-			sock_file = optarg;
+			sock_file = strdup(optarg);
 			break;
 
 		case 't':

commit 27b29554b13c05c38289aea3a0cba0856f6e15d0

Fix segfault in smcrouted -h, introduced in 575ccf2
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/smcrouted.c
@@ -19,6 +19,7 @@
 
 #include "config.h"
 
+#include <alloca.h>
 #include <err.h>
 #include <errno.h>
 #include <getopt.h>
@@ -277,13 +278,19 @@ static int compose_paths(void)
 
 static int usage(int code)
 {
-        char pidfn[sizeof(RUNSTATEDIR) + strlen(pid_file) + 6];
+        char *pidfn;
+	size_t len;
 
 	compose_paths();
+	len = sizeof(RUNSTATEDIR) + strlen(pid_file) + 6;
+	pidfn = malloc(len);
+	if (!pidfn)
+		err(EX_OSERR, "Failed allocating memory for PID file name");
+
 	if (pid_file[0] != '/')
-		snprintf(pidfn, sizeof(pidfn), "%s/%s.pid", RUNSTATEDIR, pid_file);
+		snprintf(pidfn, len, "%s/%s.pid", RUNSTATEDIR, pid_file);
 	else
-		snprintf(pidfn, sizeof(pidfn), "%s", pid_file);
+		snprintf(pidfn, len, "%s", pid_file);
 
 	printf("Usage: %s [hnNsv] [-c SEC] [-d SEC] [-e CMD] [-f FILE] [-l LVL] "
 #ifdef ENABLE_MRDISC
@@ -320,6 +327,7 @@ static int usage(int code)
 	       "  -v              Show program version\n"
 	       "\n", prognm, conf_file, ident, pidfn, sock_file);
 
+	free(pidfn);
 	cleanup();
 
 	return code;