ea9e03bbc7c2d9132d135cb11504a7a4da17a94e
Joachim Wiberg <troglobit@gmail.com>
2021-08-22 08:35:03 +0200

    Add missing NotifyAccess=main
     - Restore lost NotifyAccess=main from PR #155
     - Minor whitespace fixes for readability and consistency
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/systemd.c
+++ src/systemd.c
@@ -4,8 +4,7 @@
 
 void systemd_notify_ready(const char *status)
 {
-	sd_notifyf(0, "READY=1\nSTATUS=%s\nMAINPID=%lu\n",
-		   status, (unsigned long)getpid());
+	sd_notifyf(0, "READY=1\nSTATUS=%s\nMAINPID=%lu\n", status, (unsigned long)getpid());
 }
 
 void systemd_notify_reload(const char *status)
